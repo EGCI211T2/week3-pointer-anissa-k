b19a42815a93727251fd1b5ed73c352c903dd65a
anissa-k <anissa.kuu@student.mahidol.edu>
2026-01-29 17:18:54 +0000

    hello

--- main.cpp
+++ main.cpp
@@ -1,38 +1,47 @@
 #include <iostream>
-#include <iomanip>
-#define SIZE 10
-
 using namespace std;
 
-int main(int argc,char *argv[]){
-  int *pa, *pb, i, temp;
-int a[SIZE] ={1,2,3,4,5,6,7,8,9,10}; 
-pa = &a[0]; //pa =a;
-cout<<"Original: ";
-for (i=0; i<SIZE-1; i++ ,pa++){
-   cout<<setw(3)<<*pa ;//<<endl;
-  //cout<<pa<<endl;
-}
-cout<<setw(3)<<*pa<<endl;
-
-pa = &a[0];  
-pb = &a[SIZE-1];
-for (i=0; i<SIZE/2; i++)
+int main(int argc, char *argv[])
 {
-   temp = *pa;  
-   *pa = *pb;  
-   *pb = temp;
-   pa++;  pb--;
-}
-pa=&a[0];
-pb-=SIZE/2;
-cout<<"Reversed: ";
-for (i=0; i<SIZE-1; i++ ,pa++){
-   cout<<setw(3)<<*pa ;//<<endl;
-  //cout<<pa<<endl;
-}
-cout<<setw(3)<<*pa<<endl;
+    int i;
+    int n = argc - 1;
 
+    int* pa = new int[n];
+    int* start = pa;
 
-return 0;
+    for (i = 0; i < n; i++)
+    {
+        *pa = atoi(argv[i + 1]);
+        pa++;
+    }
+
+    pa = start;
+
+    cout << "Original: ";
+    for (i = 0; i < n; i++)
+    {
+        cout << *pa << " ";
+        pa++;
+    }
+    cout << endl;
+
+    pa = start;
+
+    for (i = 0; i < n / 2; i++)
+    {
+        int temp = pa[i];
+        pa[i] = pa[n - 1 - i];
+        pa[n - 1 - i] = temp;
+    }
+
+    cout << "Reversed: ";
+    for (i = 0; i < n; i++)
+    {
+        cout << pa[i] << " ";
+    }
+    cout << endl;
+
+    delete[] start;
+
+    return 0;
 }
